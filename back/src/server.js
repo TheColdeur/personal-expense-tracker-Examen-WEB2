227c9cd626883360498c21796832e94059281e2a
harensoa259 <narovanaharentsoa@gmail.com>
2025-09-11 09:12:23 +0300

    feat: add revenue backend with routes and controllers

--- back/src/server.js
+++ back/src/server.js
@@ -3,7 +3,9 @@ import cors from "cors"
 import dotenv from 'dotenv'
 import authRoutes from "./routes/authRoutes.js"
 import categoryRoutes from "./routes/categoryRoutes.js"
-import receiptRoutes from "./routes/receiptRoutes.js";
+import receiptRoutes from "./routes/receiptRoutes.js"
+import revenueRoutes from "./routes/revenueRoutes.js"
+import revenueReceiptRoutes from "./routes/revenueReceiptRoutes.js"
 
 dotenv.config()
 
@@ -13,11 +15,11 @@ const PORT = process.env.PORT
 app.use(cors())
 app.use(json())
 
-//Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/receipts", receiptRoutes);
-
+app.use("/api/revenues", revenueRoutes);
+app.use("/api/revenue-receipts", revenueReceiptRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at : http://localhost:${PORT}`);
