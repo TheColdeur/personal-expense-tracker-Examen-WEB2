e3df6744a28050daa5f23952937b661d415878d2
novahMax2552 <hei.vahatra@gmail.com>
2025-09-12 13:44:55 +0300

    add dashboard and expenses management

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/NavBar';
+import Sidebar from './components/layout/SideBar';
 import Dashboard from './components/dashboard/DashBoard';
+import ExpensesPage from './components/expenses/ExpensesPages';
+import ExpensesFormPage from './components/expenses/ExpenseFormPage';
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard/>} />
-    </Routes>
+    <>
+      <Navbar />
+      <Sidebar />
+      <div className="ml-72 mt-32 p-6">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/ajouter" element={<ExpensesFormPage />} />
+        </Routes>
+      </div>
+    </>
   );
 }
+
+export default App;
