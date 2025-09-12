58485df8a1c2aeb9dd5fdb7607a9e302c6879776
Rakotoson Avisoa Morgan <hei.avisoa@gmail.com>
2025-09-12 13:39:42 +0300

    Debug income routing

--- back/src/routes/incomeReceiptRoutes.js
+++ back/src/routes/incomeReceiptRoutes.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { authentication } from "../middlewares/authentication.js";
 import { 
-    uploadRevenueReceipt, 
-    downloadRevenueReceipt, 
-    deleteRevenueReceipt 
+    uploadIncomeReceipt, 
+    downloadIncomeReceipt, 
+    deleteIncomeReceipt 
 } from "../controllers/incomeReceiptController.js";
 import { upload } from "../middlewares/upload.js";
 
 const router = express.Router();
 
-router.post("/:idRevenue/upload", authentication, upload.single('receipt'), uploadRevenueReceipt);
+router.post("/:idRevenue/upload", authentication, upload.single('receipt'), uploadIncomeReceipt);
 
-router.get("/:idRevenue/download", authentication, downloadRevenueReceipt);
+router.get("/:idRevenue/download", authentication, downloadIncomeReceipt);
 
-router.delete("/:idRevenue/receipt", authentication, deleteRevenueReceipt);
+router.delete("/:idRevenue/receipt", authentication, deleteIncomeReceipt);
 
 export default router;
